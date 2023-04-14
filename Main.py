e6d34204b06ad74b8a0a9bf4de901ee0e29ecba1
BuildTools <unconfigured@null.spigotmc.org>
2023-04-14 11:49:07 +0300

    Updated Main.py

--- Main.py
+++ Main.py
@@ -9,4 +9,5 @@ Ingore this one. This is just an example / test file to test the ShapeHandler
 
 sh.Circle(34, True, 20, "green", True)
 sh.Square(200, True, 3, "blue", True)
+sh.Triangle(200, True, 3, "blue", True)
 input()
