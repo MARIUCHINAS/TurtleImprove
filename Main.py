aca462d80fd55e2d0b9be3d85f6715c2c857258f
BuildTools <unconfigured@null.spigotmc.org>
2023-04-14 11:44:32 +0300

    Added warning to Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,12 @@
 import turtle
 import ShapeHandler as sh
 
+"""
+Ingore this one this is just a example / test file to test the ShapeHandler
+
+"""
+
+
 sh.Circle(34, True, 20, "green", True)
 sh.Square(200, True, 3, "blue", True)
 input()
